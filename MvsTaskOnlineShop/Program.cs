5b2754c4aa9b4085ded054e3936c3db9f45394d6
Nicat Huseynov <nicat.huseynov.m@gmail.com>
2024-04-05 02:49:26 +0400

    OnlineShop is created

---
+++ MvsTaskOnlineShop/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Builder;
+
+namespace MvsTaskOnlineShop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddSession(option =>
+            {
+                option.IdleTimeout = TimeSpan.FromMinutes(60);
+                option.Cookie.HttpOnly = true;
+                option.Cookie.IsEssential = true;
+            });
+            builder.Services.AddRazorPages();
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+            app.UseSession();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}");
+            });
+
+            app.Run();
+        }
+    }
+}
